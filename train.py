092af7dd91c6f46009c7ae153a6cf86eb7b1ac7a
Vinh2608 <vinhluu1996@gmail.com>
2023-07-19 16:46:42 +0700

    train.py with outlier remove from dataset

--- train.py
+++ train.py
@@ -37,12 +37,13 @@ def train(model, loss_func, device, train_loader, optimizer, loss_optimizer, epo
             print("Epoch {} Iteration {}: Loss = {}".format(epoch, batch_idx, loss))
             if loss < best_loss:
                 best_loss = loss
-                PATH = f'/content/SubCenterArcFace/checkpoints/{config.model}_model_s={s}_m={m}_{best_loss}_{epoch}_arcfaceloss73_train.pt'
+                PATH = f'/content/SubCenterArcFace/checkpoints/{config.model}_model_s={s}_m={m}_{best_loss}_{epoch}_subcenter_train.pt'
                 torch.save({
                     'epoch': epoch,
                     'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict(),
-                    'loss': best_loss,
+                    'loss_optimizer_state_dict': loss_optimizer.state_dict(),
+                    'loss': loss_func.parameters(),
                 }, PATH)
 
 ### convenient function from pytorch-metric-learning ###
@@ -63,14 +64,6 @@ def test(train_set, test_set, model, accuracy_calculator):
         test_embeddings, test_labels, train_embeddings, train_labels, False
     )
     print("Test set accuracy (Precision@1) = {}".format(accuracies["precision_at_1"]))
-    if accuracies["precision_at_1"] > best_acc:
-        best_acc = accuracies["precision_at_1"]
-        PATH = f'/content/SubCenterArcFace/checkpoints/{config.model}_model_s={s}_m={m}_{best_loss}_acc{best_acc}_{epoch}_arcfaceloss73_test.pt'
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'loss': best_loss,
-        }, PATH)
 
 
 if __name__ == '__main__':
@@ -95,6 +88,9 @@ if __name__ == '__main__':
     elif config.model == 'iresnet50':
         model = get_model("r50", fp16=False)
         model.load_state_dict(checkpoint)
+    elif config.model == 'iresnet34':
+        model = get_model("r34", fp16=False)
+        model.load_state_dict(checkpoint)
     else:
         model = MobileFaceNet(512).to(device)
         # model_dict = model.state_dict()
@@ -118,26 +114,41 @@ if __name__ == '__main__':
     train_dataset = Dataset('VN-celeb_align_frontal_full', 'label_train.txt', phase='train', input_shape=(3, 112, 112))
     test_dataset = Dataset('VN-celeb_align_frontal_full', 'label_test.txt', phase='test', input_shape=(3, 112, 112))
     train_loader = data.DataLoader(train_dataset,
-                                   batch_size=16,
+                                   batch_size=100,
                                    shuffle=True,
                                    num_workers=4)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=50)
 
     optimizer = optim.Adam(model.parameters(), lr=0.1)
     #optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    initial_epoch = 0
+    initial_epoch = 0#checkpoint['epoch']
     num_epochs = 100
 
     model.to(device)
     ### pytorch-metric-learning stuff ###
     loss_func = losses.SubCenterArcFaceLoss(num_classes=1021, embedding_size=512, margin=m, scale=m).to(device)
-    loss_optimizer = torch.optim.Adam(loss_func.parameters(), lr=1e-4)
+    loss_optimizer = torch.optim.Adam(loss_func.parameters(), lr=1e-2)
     accuracy_calculator = AccuracyCalculator(include=("precision_at_1",), k=1)
     ### pytorch-metric-learning stuff ###
-    for epoch in range(initial_epoch, initial_epoch+ num_epochs + 1):
+    for epoch in range(initial_epoch, initial_epoch + num_epochs + 1):
         train(model, loss_func, device, train_loader, optimizer, loss_optimizer, epoch)
         test(train_dataset, test_dataset, model, accuracy_calculator)
+    
     train_embeddings, train_labels = get_all_embeddings(train_dataset, model)    
-    outliers, _ = loss_func.get_outliers(train_embeddings, train_labels.squeeze(1))
-    print(f"There are {len(outliers)} outliers")
-    torch.save({'outlier': outliers},'/content/SubCenterArcFace/outliers/outlier.pt' )
+    outliers1, _ = loss_func.get_outliers(train_embeddings, train_labels.squeeze(1))
+    print(f"There are {len(outliers1)} outliers")
+    
+    for i in range(len(outliers1)):
+        del train_dataset.imgs[outliers1[i]]
+
+    test_embeddings, test_labels = get_all_embeddings(test_dataset, model)
+    outliers2, _ = loss_func.get_outliers(test_embeddings, test_labels.squeeze(1))
+    print(f"There are {len(outliers2)} outliers")
+
+    for i in range(len(outliers2)):
+        del test_dataset.imgs[outliers2[i]]    
+        
+    torch.save({
+        'train_dataset_ls': train_dataset.imgs,
+        'test_dataset_ls': test_dataset.imgs
+        },'/content/SubCenterArcFace/outliers/train_test_remove_outlier.pt' )
